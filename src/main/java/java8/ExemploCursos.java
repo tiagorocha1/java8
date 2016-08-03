e97073f6df8fc6a983226882b4c49ef21aafda10
Tiago Rocha <tiagoferreira.info@gmail.com>
2016-08-03 17:31:21 -0300

    stream

--- src/main/java/java8/ExemploCursos.java
+++ src/main/java/java8/ExemploCursos.java
@@ -1,6 +1,7 @@
 package java8;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -36,6 +37,21 @@ public class ExemploCursos {
 		cursos.add(new Curso("JavaScript", 150));
 		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		
+		System.out.println("Cursos com pelo menos 100 alunos matriculados:");
+		cursos.stream()
+			.filter( c -> c.getAlunos() >= 100)
+			.forEach(c -> System.out.println(c.getNome()));
+		
+		int sum = cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.mapToInt(Curso::getAlunos)
+			.sum();	
+		
+		System.out.println("**********************");
+		System.out.println("Total de Alunos: "+sum);
 
 	}
 
